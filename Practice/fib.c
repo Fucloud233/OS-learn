0ab738a1dfab6478b4f6bf034f5f35a6bc2a1a38
Fucloud <270928474@qq.com>
2023-04-06 21:41:14 +0800

    添加 练习文件

---
+++ Practice/fib.c
@@ -0,0 +1,38 @@
+#include <pthread.h>
+#include <stdio.h>
+
+void *fib(void* n_pointer) {
+	int n = *(int *)n_pointer;
+
+	if(n<=1)
+		pthread_exit((void*)1);
+	else {
+		pthread_t thread1, thread2;
+		int n1 = n-1, n2 = n-2;	
+		// printf("n1 = %d, n2 = %d\n", n1, n2);
+
+		// 创建线程
+		pthread_create(&thread1, NULL, fib, (void *)&n1);
+		pthread_create(&thread2, NULL, fib, (void *)&n2);
+		
+		int ret1, ret2;		
+		// 等待线程
+		pthread_join(thread1, (void *)&ret1);
+		pthread_join(thread2, (void *)&ret2);		
+		
+		// 退出进程
+		int result = ret1 + ret2;
+		// printf("MidRes: %d\n", result);
+		pthread_exit((void*)(result));
+	}
+}
+
+int main(){
+	pthread_t thread;
+	int n = 5, ret;
+	// printf("Main Thread Running\n");
+	pthread_create(&thread, NULL, fib, (void *) &n);
+	pthread_join(thread, (void *)&ret);
+	printf("fib(%d) = %d\n", n, ret);	
+
+}
---
+++ Practice/fib_v3.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+int thread_num = 0;
+int max_thread_num = 0;
+
+int fib(int n) {
+	if(n<=1)
+		return 1;
+	else
+		return fib(n-1) + fib(n-2);
+}
+
+void *fib_thread(void *);
+
+int create_thread(pthread_t *thread, int *argv) {
+	if(thread_num == max_thread_num)
+		return 0;
+	pthread_create(thread, NULL, fib_thread, (void *)argv);
+	thread_num++;
+	return 1;
+}
+
+int wait_thread(pthread_t *thread) {
+	int ret_value;
+	pthread_join(*thread, (void *)&ret_value);
+	thread_num--;
+	return ret_value;
+}
+
+void *fib_thread(void* n_pointer) {
+	int n = *(int *)n_pointer;
+
+	if(n<=1) 
+		pthread_exit((void *)1);
+	else {
+		pthread_t thread1, thread2;
+		// 创建线程
+		int flag1, flag2;
+		int n1 = n-1, n2 = n-2;
+		flag1 = create_thread(&thread1, &n1);
+		flag2 = create_thread(&thread2, &n2);
+		
+		// 等待线程结束
+		// 如果没有创建线程 则调用递归的fib解决
+		int ret1, ret2;
+		if (flag1)
+			pthread_join(thread1, (void *)&ret1);
+		else
+			ret1 = fib(n1);
+		if (flag2) 
+			pthread_join(thread2, (void *)&ret2);
+		else
+			ret2 = fib(n2);
+		// 返回 最终结果
+		int res = ret1 + ret2;
+		pthread_exit((void *)res);
+	}
+	thread_num--;
+}
+
+int main(int argc, char* argv[]) {
+	if (argc!=3) {
+		printf("usage:%s <n> <pthread number>\n", argv[0]);
+		exit(1);
+	}
+
+	int n = atoi(argv[1]);
+	max_thread_num = atoi(argv[2]) + 1;
+	
+	int res;
+
+	struct timeval start_time, end_time;
+	double exec_time;
+	pthread_t thread;
+	
+	gettimeofday(&start_time, NULL);
+	pthread_create(&thread, NULL, fib_thread, (void *)&n);
+	pthread_join(thread, (void *)&res);
+	gettimeofday(&end_time, NULL);
+	
+	exec_time = (end_time.tv_sec - start_time.tv_sec) * 1000.0;
+	exec_time += (end_time.tv_usec - start_time.tv_usec) / 1000.0;
+
+//	printf("Result: %3d, (%.3fms)\n", res, exec_time); 
+	printf("%.3f", exec_time);
+}
