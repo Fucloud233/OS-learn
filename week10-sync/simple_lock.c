69eb89a81518d4056d66492c2f452859b472bf52
Fucloud <270928474@qq.com>
2023-04-25 20:03:50 +0800

    实现 Task1

--- week10-sync/simple_lock.c
+++ week10-sync/simple_lock.c
@@ -2,33 +2,50 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-iint global_resource;
+int global_resource;
 
 // 使用加法
 void* thread_inc (void* arg) {
-    for (int i = 0; i < 10; i++) {
+    pthread_mutex_t* mutexl = (pthread_mutex_t *)arg;
+	
+	// lock
+	pthread_mutex_lock(mutexl);
+    
+	for (int i = 0; i < 10; i++) {
         global_resource++;
         printf("Increment: %d\n", global_resource);
         sleep(1);
-    }
+    } 
     printf("Thread inc finished.\n");
-    return NULL;
+ 
+	// unlock
+	pthread_mutex_unlock(mutexl);
+	return NULL;
 }
 
 // 使用减法
 void* thread_dec (void* arg) {
+	pthread_mutex_t* mutexl = (pthread_mutex_t *)arg;
+	
+	// lock
+	pthread_mutex_lock(mutexl);
     for (int i = 0; i < 10; i++) {
         global_resource--;
         printf("Decrement: %d\n", global_resource);
     }
     printf("Thread dec finished.\n");
+
+	// unlock
+	pthread_mutex_unlock(mutexl);
     return NULL;
 }
 
 int main (int argc, char** argv) {
-    pthread_t threads[2];
-    pthread_create(&threads[0], NULL, thread_inc, NULL);
-    pthread_create(&threads[1], NULL, thread_dec, NULL);
+	pthread_mutex_t mutexl = PTHREAD_MUTEX_INITIALIZER;
+	
+	pthread_t threads[2];
+    pthread_create(&threads[0], NULL, thread_inc, (void *)&mutexl);
+    pthread_create(&threads[1], NULL, thread_dec, (void *)&mutexl);
 
     for (int i = 0; i < 2; i++) {
         pthread_join(threads[i], NULL);
